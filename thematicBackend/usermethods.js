359264bfe10715a0b784bed4336c403ddea91ab3
Wills <lukemw25@gmail.com>
2024-03-19 19:55:30 +0000

    userAdd/Remove

--- thematicBackend/usermethods.js
+++ thematicBackend/usermethods.js
@@ -1,32 +1,35 @@
-const User = require("./models/user");
 const mongoose = require("mongoose");
+const User = require("./models/user");
 const config = require("./config");
 
 const mongoURI = config.mongoURI;
 console.log("mongoURI", mongoURI);
 
 // Connect to MongoDB
-mongoose.connect(mongoURI);
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 const db = mongoose.connection;
-
-async function addUser(name, email, password) {
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+async function addUser(Username, Email, Password) {
   try {
-    // check if user with the same email already exists
-    const existingUser = await User.findOne({ email });
-
+    // Check if a user with the same email already exists
+    const existingUser = await User.findOne({ Email }); // Ensure field names are lowercase if your model defines them that way
     if (existingUser) {
       throw new Error('User with this email already exists');
     }
 
-    // create new user document
+    // Create new user document
     const newUser = new User({
-      Username: name,
-      Email: email,
-      Password: password,
+      Username, // Make sure these match the case and spelling of your schema definitions
+      Email,
+      Password,
     });
 
-    // save user to the database
+    // Save user to the database
     const savedUser = await newUser.save();
+    console.log("User added:", savedUser);
     return savedUser;
   } catch (err) {
     console.error(err);
@@ -34,17 +37,18 @@ async function addUser(name, email, password) {
   }
 }
 
-async function removeUser(email) {
-  try {
-    // check if user with the given email exists
-    const existingUser = await User.findOne({ email });
 
+async function removeUser(Email) {
+  try {
+    // Check if a user with the given email exists
+    const existingUser = await User.findOne({ Email });
     if (!existingUser) {
       throw new Error('User with this email does not exist');
     }
 
-    // remove the user from the database
-    const removedUser = await User.findOneAndRemove({ email });
+    // Remove the user from the database
+    const removedUser = await User.findOneAndDelete({ Email });
+    console.log("User removed:", removedUser);
     return removedUser;
   } catch (err) {
     console.error(err);
@@ -52,16 +56,21 @@ async function removeUser(email) {
   }
 }
 
-addUser("luke", "luke@email", "password")
-  .then((user) => console.log("User added:", user))
-  .catch((err) => console.error(err));
+async function testRemoveUser() {
+  try {
+    // Add a user
+    const user = await addUser("test", "test@email.com", "password");
+    console.log("User added:", user);
 
-addUser("tasha", "tasha@email", "password")
-  .then((user) => console.log("User added:", user))
-  .catch((err) => console.error(err));
+    // Remove the user
+    const removedUser = await removeUser("test@email.com");
+    console.log("User removed:", removedUser);
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-removeUser("luke@email")
-  .then((removedUser) => console.log('User removed:', removedUser))
-  .catch((err) => console.error(err));
+testRemoveUser();
 
-module.exports = { addUser, removeUser };
+// addUser("luke", "l@gmail.com", "password");
+removeUser("tasha@gmail");
