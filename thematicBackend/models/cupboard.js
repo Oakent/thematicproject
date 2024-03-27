c5f2f8d515bcde9435c15bae97a5899fe28052d0
oakent <oakentravis@gmail.com>
2024-03-27 17:29:46 +0000

    reference cupboard in user schema

--- thematicBackend/models/cupboard.js
+++ thematicBackend/models/cupboard.js
@@ -5,7 +5,6 @@ const { ingredientSchema } = require("./ingredient");
 const Schema = mongoose.Schema;
 
 const CupboardSchema = new Schema({
-  owner: { type: Schema.Types.ObjectId, ref: "User" },
   ingredients: [{ type: Schema.Types.ObjectId, ref: "Ingredient" }],
 });
 
--- thematicBackend/models/user.js
+++ thematicBackend/models/user.js
@@ -6,16 +6,6 @@ const UserSchema = new Schema({
   Username: { type: String, required: true, minLength: 3, maxLength: 25 },
   Email: { type: String, required: true, minLength: 5, maxLength: 100 },
   Password: { type: String, required: true, minLength: 8, maxLength: 100 },
- 
-  personalIngredients: [{
-    type: Object,
-    Name: { type: String, required: true },
-    Unit: { type: String, required: true },
-    Category: { type: String, required: true },
-    Allergens: { type: String, required: true }
-  }]
+  Cupboard: {type: Schema.Types.ObjectId, required: true, ref:'Cupboard'}
 });
-
-
-
 module.exports = mongoose.model("User", UserSchema);
