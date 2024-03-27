ff9d4ec5a82cca28eac4d1bc4175b83147a1dff2
oakent <oakentravis@gmail.com>
2024-03-27 19:21:13 +0000

    reference static resources within index.html

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-const catalogRouter = require("./routes/catalog");
+const recipeRouter = require("./routes/recipe.js");
 
 var app = express();
 
@@ -20,9 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/catalog", catalogRouter);
+app.use("/", recipeRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
