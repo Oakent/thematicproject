a279e1aa1cd3f9d379c7deef4659e00bb2d85111
oakent <oakentravis@gmail.com>
2024-03-30 06:46:41 +0000

    add ingredients to update cupboard controller

--- controllers/cupboardController.js
+++ controllers/cupboardController.js
@@ -1,4 +1,5 @@
 const Cupboard = require("../models/cupboard");
+const Ingredients = require("../models/ingredient");
 const asyncHandler = require("express-async-handler");
 
 exports.cupboardGet = asyncHandler(async (req, res) => {
@@ -6,5 +7,6 @@ exports.cupboardGet = asyncHandler(async (req, res) => {
 });
 
 exports.cupboardUpdate = asyncHandler(async (req, res) => {
-  res.send("not implemented, update cupboard");
+  const allIngredients = await Ingredients.find();
+  res.render("cupboardUpdate", allIngredients);
 });
