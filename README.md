ac41c676aa95f7ab8b01fb400928ade9d4510fac
oakent <oakentravis@gmail.com>
2024-04-02 17:55:12 +0100

    embed cupboard within user schema

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ SSR Pages: registration, recipe details, recipe search page, user profile
 CSR: user cupboard
 
 User profile page: personal info, dietary restrictions COULD: recipe bookmarks, recipe history
+
--- models/user.js
+++ models/user.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
-
+const ingredientSchema = require("./ingredient");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   Username: { type: String, required: true, minLength: 3, maxLength: 25 },
   Email: { type: String, required: true, minLength: 5, maxLength: 100 },
   Password: { type: String, required: true, minLength: 8, maxLength: 100 },
-  Cupboard: {type: Schema.Types.ObjectId, required: true, ref:'Cupboard'}
+  Cupboard: [
+    { type: Schema.Types.ObjectId, required: true, ref: "Ingredient" },
+  ],
 });
 module.exports = mongoose.model("User", UserSchema);
