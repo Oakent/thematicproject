46ec3422b738540152746cbb6c13f0e3007c1b32
oakent <oakentravis@gmail.com>
2024-04-03 14:55:27 +0100

    add recipe id page

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const asyncHandler = require("express-async-handler");
 const Recipe = require("../models/recipe");
 
-exports.recipesGet = asyncHandler(async (req, res) => {
+exports.recipesGet = asyncHandler(async (req, res, next) => {
   const allRecipes = await Recipe.find({}).sort({ title: 1 }).exec();
   res.render("recipes", { recipes: allRecipes });
 });
@@ -11,8 +11,11 @@ exports.recipeCreate = asyncHandler(async (req, res) => {
   res.send("not implemented, recipe create");
 });
 
-exports.recipeGetById = asyncHandler(async (req, res) => {
-  res.send("not implemented, recipe get by id");
+exports.recipeGetById = asyncHandler(async (req, res, next) => {
+  console.log("body id:" + req.params._id);
+  const recipe = await Recipe.findById(req.params._id).exec();
+
+  res.render("recipe_page", { recipe: recipe });
 });
 
 exports.recipeUpdate = asyncHandler(async (req, res) => {
