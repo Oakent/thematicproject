2bd3c0e47c514adaecc42cd7aa3376b08e4fac55
oakent <oakentravis@gmail.com>
2024-04-03 18:21:06 +0100

    add ingredient form reads properties from mongodb

--- controllers/ingredientController.js
+++ controllers/ingredientController.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
 const Ingredient = require("../models/ingredient");
+const ingredient = require("../models/ingredient");
 
 exports.ingredientsGet = asyncHandler(async (req, res) => {
   res.send("not implemented, ingredients get");
 });
 
 exports.add_ingredient_get = asyncHandler(async (req, res, next) => {
-  res.render("add_ingredient");
+  const ingredients = await Ingredient.find().lean();
+
+  // Extract unique units, categories, and allergens
+  const unitsSet = new Set(ingredients.map((ingredient) => ingredient.unit));
+  const categoriesSet = new Set(
+    ingredients.map((ingredient) => ingredient.category)
+  );
+  let allergensSet = new Set();
+  ingredients.forEach((ingredient) => {
+    ingredient.allergens.forEach((allergen) => {
+      allergensSet.add(allergen);
+    });
+  });
+
+  const units = Array.from(unitsSet);
+  const categories = Array.from(categoriesSet);
+  const allergens = Array.from(allergensSet);
+  res.render("add_ingredient", {
+    allergens: allergens,
+    units: units,
+    categories: categories,
+  });
 });
 
 exports.add_ingredient_post = asyncHandler(async (req, res) => {
