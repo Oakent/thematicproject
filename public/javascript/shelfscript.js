245d96522882e12fe16a560527b4cb74345130a8
Jacob Wallis <22513465@stu.mmu.ac.uk>
2024-04-04 17:52:51 +0100

    added html to edit cupboard

--- public/javascript/shelfscript.js
+++
@@ -1,9 +0,0 @@
-let express = require("express");
-let app = app.js;
-let path = require("path");
-
-app.get("/",(req,res) => {
-    res.sendFile(path.join(__dirname,'public'));
-});
-
-app.listen(3000);
