fc67bb074a985e3f09bca4eeaf83265fd9bd8f47
oakent <oakentravis@gmail.com>
2024-04-07 15:47:25 +0100

    make ingredient names case insensitive

--- models/ingredient.js
+++ models/ingredient.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const IngredientSchema = new Schema({
-  name: { type: String, required: true, unique: true },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    collation: { locale: "en", strength: 2 },
+  },
   unit: { type: String, required: false, default: "g" },
   category: { type: String, required: false, default: "other" },
   allergens: { type: [String], required: false, default: ["unknown"] },
